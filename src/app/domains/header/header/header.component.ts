6433e7f8b15b2fe6b30ea57c7f30891dba23f9e5
Mn1kaa <ulisesalejandro98@hotmail.com>
2024-11-15 16:34:41 -0600

    Primer Commit Turing-IA_Apertura Proyecto

---
+++ src/app/domains/header/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import {MatGridListModule} from '@angular/material/grid-list'
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+
+}
